37bc616dc68f0087d1efd41701f62ca7a3304c41
Julien <lemenicier_julien@yahoo.fr>
2021-07-09 16:22:57 +0200

    test in main.py

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
+from classes.Chain import Chain
 from classes.Wallet import Wallet
+from classes.Block import Block
+#from classes.Chain import Chain
 
 
 def main():
+    # test main
     print("Hello World!")
-    #wallet = Wallet()
-    #wallet.generate_unique_id()
-    #wallet.save()
-    #print(wallet.unique_id)
+
+    # test creation objet wallet
+    # wallet0 = Wallet()
+    # wallet0.generate_unique_id()
+    # wallet0.save()
+    # print(wallet0.unique_id)
+    # wallet0.balance = 200
+
+    # test methode load()
     wallet = Wallet()
     wallet.load("a7afe60a-a194-4e3a-855f-0a557e8de665")
     print(wallet.unique_id)
+
+    # test bloc generate_hash to implement !!!
+    # bloc = Block()
+    # bloc.load()
+    # print(bloc.get_weight)
+
+    #test verify_hash
+    chain = Chain()
+    chain.add_block()
+    print(chain)
 
 
 if __name__ == "__main__":
