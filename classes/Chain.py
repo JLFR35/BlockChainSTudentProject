a4aed41637f4684f607032b482dd66b10210e084
Julien <lemenicier_julien@yahoo.fr>
2021-07-09 15:18:43 +0200

    add "add_block())" & get_block()"

--- classes/Chain.py
+++ classes/Chain.py
@@ -1,19 +1,26 @@
 import hashlib
 import os
 
+from classes.Block import Block
+
+
 class Chain:
 
     def __init__(self):
         self.blocks = []
         self.last_transaction_number = 0
+        self.hash = hash
 
-    def generate_hash(self, bloc_hashed=None):
-        number = 0
-        hash = hashlib.sha256("number".encode()).hexdigest()
-        while self.verify_hash(bloc_hashed):
-            number += 1
-            block_hash = self.blocks
+    # à refaire
+    # def generate_hash(self, bloc_hashed):
+    #     number = 0
+    #     hash = hashlib.sha256("number".encode()).hexdigest()
+    #     while not self.verify_hash(bloc_hashed):
+    #         number += 1
+    #         block_hashed = self.blocks
 
+    def block_to_hash(self,number):
+        return hashlib.sha256(number.encode().hexdegiest())
 
     @staticmethod
     def verify_hash(bloc_hashed) -> bool :
@@ -24,7 +31,14 @@ class Chain:
             return True
 
     def add_block(self):
+        self.generate_hash()
+        block = Block()
+        self.blocks.append(block)
+        block.save()
 
-    def get_block(self):
+    def get_block(self, v):
+        for k in self.blocks:
+            if k["hash"] == v:
+                return k
 
     def add_transaction(self):
